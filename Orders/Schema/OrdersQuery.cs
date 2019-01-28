ec7a21b1f0be01623b130250910d7195a8388eef
Chris Phillips <chris.phillips@mindmatrix.net>
2019-01-27 22:16:01 -0500

    Update OrdersQuery.cs

--- Orders/Schema/OrdersQuery.cs
+++ Orders/Schema/OrdersQuery.cs
@@ -13,6 +13,15 @@ namespace Orders.Schema
             Name = "Query";
             Field<ListGraphType<OrderType>>("orders",
                 resolve: context => orders.GetOrdersAsync());
+
+            FieldAsync<OrderType>("order",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "orderId" }),
+                resolve: async context =>
+                {
+                    var orderId = context.GetArgument<string>("orderId");
+                    return await context.TryAsyncResolve(
+                        async c => await orders.GetOrderByIdAsync(orderId));
+                });
         }
     }
 }
